da58ff95b89d89b70d3daa33f88adf1edde8a713
Hungbanh1 <hungbg.dev1@gmail.com>
2024-11-08 15:29:18 +0700

    LAB 2

---
+++ Controllers/NguoidungController.cs
@@ -0,0 +1,118 @@
+﻿using MvcBookStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcBookStore.Controllers
+{
+    public class NguoidungController : Controller
+    {
+
+        private readonly dbQLBansachDataContext db;
+
+        public NguoidungController()
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["QLBANSACHConnectionString"].ConnectionString;
+            db = new dbQLBansachDataContext(connectionString);
+        }
+   
+        // GET: Nguoidung
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Dangnhap(FormCollection collection)
+        {
+            // Gán các giá trị người dùng nhập liệu cho các biến  
+            var tendn = collection["TenDN"];
+            var matkhau = collection["Matkhau"];
+            if (String.IsNullOrEmpty(tendn))
+            {
+                ViewData["Loi1"] = "Phải nhập tên đăng nhập";
+            }
+            else if (String.IsNullOrEmpty(matkhau))
+            {
+                ViewData["Loi2"] = "Phải nhập mật khẩu";
+            }
+            else
+            {
+                // Gán giá trị cho đối tượng được tạo mới (kh)  
+                KHACHHANG kh = db.KHACHHANGs.SingleOrDefault(n => n.Taikhoan == tendn && n.Matkhau == matkhau);
+                if (kh != null)
+                {
+                    ViewBag.Thongbao = "Chúc mừng đăng nhập thành công";
+                    Session["Taikhoan"] = kh;
+                    return RedirectToAction("Index", "Bookstore");
+                }
+                else
+                {
+                    ViewBag.Thongbao = "Tên đăng nhập hoặc mật khẩu không đúng";
+                }
+            }
+            return View();
+        }
+        [HttpGet]
+        public ActionResult Dangky()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Dangky(FormCollection collection, KHACHHANG kh)
+        {
+            // Gán các giá trị người dùng nhập liệu cho các biến  
+            var hoten = collection["HotenKH"];
+            var tenden = collection["TenDN"];
+            var matkhau = collection["Matkhau"];
+            var matkhauphai = collection["Matkhauphai"];
+            var diachi = collection["Diachi"];
+            var email = collection["Email"];
+            var dienthoai = collection["Dienthoai"];
+            var ngaysinh = string.Format("{0:MM/dd/yyyy}", collection["Ngaysinh"]);
+
+            if (String.IsNullOrEmpty(hoten))
+            {
+                ViewData["Loi1"] = "Họ tên khách hàng không được để trống";
+            }
+            else if (String.IsNullOrEmpty(tenden))
+            {
+                ViewData["Loi2"] = "Phải nhập tên đăng nhập";
+            }
+            else if (String.IsNullOrEmpty(matkhau))
+            {
+                ViewData["Loi3"] = "Phải nhập mật khẩu";
+            }
+            else if (String.IsNullOrEmpty(matkhauphai))
+            {
+                ViewData["Loi4"] = "Phải nhập lại mật khẩu";
+            }
+            if (String.IsNullOrEmpty(email))
+            {
+                ViewData["Loi5"] = "Email không được bỏ trống";
+            }
+
+            if (String.IsNullOrEmpty(dienthoai))
+            {
+                ViewData["Loi6"] = "Phải nhập điện thoại";
+            }
+            else
+            {
+                // Gán giá trị cho đối tượng được tạo mới (kh)  
+                kh.HoTen = hoten;
+                kh.Taikhoan = tenden;
+                kh.Matkhau = matkhau;
+                kh.Email = email;
+                kh.DiachiKH = diachi;
+                kh.DienthoaiKH = dienthoai;
+                kh.Ngaysinh = DateTime.Parse(ngaysinh);
+                db.KHACHHANGs.InsertOnSubmit(kh);
+                db.SubmitChanges();
+                return RedirectToAction("Dangnhap");
+            }
+            return this.Dangky();
+            //return View();
+        }
+    }
+}
---
+++ Models/Giohang.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+
+namespace MvcBookStore.Models
+{
+    public class Giohang
+    {
+        //Tạo đối tượng data chứa dữ liệu từ model dbBansach đã tạo.  
+        //dbQlbansachDataContext data = new dbQlbansachDataContext();
+        //private readonly dbQLBansachDataContext data;
+        //dbQLBansachDataContext data = new dbQLBansachDataContext();
+        //private readonly dbQLBansachDataContext data;
+        //public Giohang()
+        //{
+        //    string connectionString = ConfigurationManager.ConnectionStrings["QLBANSACHConnectionString"].ConnectionString;
+        //    db = new dbQLBansachDataContext(connectionString);
+        //}
+        //string connectionString = ConfigurationManager.ConnectionStrings["QLBANSACHConnectionString"].ConnectionString;
+
+        dbQLBansachDataContext db = new dbQLBansachDataContext(ConfigurationManager.ConnectionStrings["QLBANSACHConnectionString"].ConnectionString);
+        public int iMasach { get; set; }
+        public string sTenSach { get; set; }
+        public string sAnhbia { get; set; }
+        public Double dDongia { get; set; }
+        public int iSoluong { get; set; }
+        public Double dThanhtien
+        {
+            get { return iSoluong * dDongia; }
+        }
+
+        //Khởi tạo giỏ hàng theo Masach được truyền vào với Soluong mặc định là 1  
+        public Giohang(int Masach)
+        {
+            iMasach = Masach;
+
+            // Tìm sách bằng SingleOrDefault để tránh lỗi nếu không tìm thấy
+            SACH sach = db.SACHes.Single(n => n.Masach == iMasach);
+            if (sach != null)
+            {
+                sTenSach = sach.Tensach;
+                sAnhbia = sach.Anhbia;
+                dDongia = double.Parse(sach.Giaban.ToString());
+                iSoluong = 1; // Mặc định số lượng là 1
+            }
+            else
+            {
+                // Xử lý nếu không tìm thấy sách
+                throw new Exception("Không tìm thấy sách với mã: " + iMasach);
+            }
+        }
+    }
+}
